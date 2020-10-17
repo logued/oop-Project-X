5cbd3c5f67ae81ad4dc4d49a0296a8d0c62c2626
Dermot <dermot.logue@dkit.ie>
2020-10-17 11:24:34 +0100

    Added sum() function

--- src/main/java/org/dkit/oop/App.java
+++ src/main/java/org/dkit/oop/App.java
@@ -10,5 +10,11 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("An addition to the App.java source file");
+        int total = sum( 2,3);
+        System.out.println("Total = " + total);
+    }
+
+    public static int sum( int x, int y){
+        return x + y;
     }
 }
